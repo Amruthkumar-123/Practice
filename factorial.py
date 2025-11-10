2b97514a1597689d6067cb92a073e59aec8fd6c0
Amruthkumar-123 <amruthkumarronanki@gmail.com>
2025-11-10 21:28:04 +0530

    create factorial

---
+++ factorial.py
@@ -0,0 +1,8 @@
+num = int(input("Enter a non-negative integer: "))
+factorial = 1
+if num < 0:
+    print("Factorial is not defined for negative numbers.")
+else:
+    for i in range(1, num + 1):
+        factorial *= i
+    print(f"Factorial of {num} is {factorial}")
